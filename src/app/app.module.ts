ec46c59dce15390608d4de1fec0351151d19bfd9
khalilbego1 <kemo5330@gmail.com>
2019-05-12 13:26:13 +0200

    4 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { WrapperComponent } from './wrapper/wrapper.component';
+import { ChangeStatusComponent } from './change-status/change-status.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,NgbModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule, NgbModule],
+  declarations: [ AppComponent, HelloComponent, WrapperComponent, ChangeStatusComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
